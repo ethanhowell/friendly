0f6ec702724af2c9c6fa526f6b4a69ae2adc43b7
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-13 12:30:16 -0500

    created view for RegisterActivity

--- app/src/main/java/com/ethanjhowell/friendly/activities/RegisterActivity.java
+++ app/src/main/java/com/ethanjhowell/friendly/activities/RegisterActivity.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.ethanjhowell.friendly.R;
+import com.ethanjhowell.friendly.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        ActivityRegisterBinding binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
