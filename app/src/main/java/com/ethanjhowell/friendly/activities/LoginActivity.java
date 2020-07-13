95494b43da661a7f06252b4b211526b340af2fc1
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-13 16:49:57 -0500

    facebook is kind of working, but not really

--- app/src/main/java/com/ethanjhowell/friendly/activities/LoginActivity.java
+++ app/src/main/java/com/ethanjhowell/friendly/activities/LoginActivity.java
@@ -11,6 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.ethanjhowell.friendly.databinding.ActivityLoginBinding;
 import com.parse.ParseUser;
+import com.parse.facebook.ParseFacebookUtils;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
     private static String TAG = LoginActivity.class.getCanonicalName();
@@ -28,6 +31,19 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(new Intent(this, GroupActivity.class));
             finish();
         }
+
+        binding.btFacebookLogin.setOnClickListener(v -> ParseFacebookUtils.logInWithReadPermissionsInBackground(this, Arrays.asList(), (user, e) -> {
+            if (user == null) {
+                Log.d(TAG, "Uh oh. The user cancelled the Facebook login.");
+            } else {
+                if (user.isNew()) {
+                    Log.d(TAG, "User signed up and logged in through Facebook!");
+                }
+                Log.d(TAG, "User logged in through Facebook!");
+                startActivity(new Intent(this, GroupActivity.class));
+                finish();
+            }
+        }));
 
         binding.btLogin.setOnClickListener(v -> ParseUser.logInInBackground(binding.etUsername.getText().toString(),
                 binding.etPassword.getText().toString(),
@@ -51,6 +67,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
         Log.d(TAG, String.format("onActivityResult: requestCode = %d, resultCode = %d", requestCode, resultCode));
         if (requestCode == REGISTER_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
