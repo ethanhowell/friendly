009aec989bce743de206d4ce2c8798fa4b701f53
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-14 14:52:04 -0500

    user can now take a photo using phone's camera

--- app/src/main/java/com/ethanjhowell/friendly/activities/NewUserActivity.java
+++ app/src/main/java/com/ethanjhowell/friendly/activities/NewUserActivity.java
@@ -3,13 +3,17 @@ package com.ethanjhowell.friendly.activities;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.CustomTarget;
@@ -17,8 +21,14 @@ import com.bumptech.glide.request.transition.Transition;
 import com.ethanjhowell.friendly.databinding.ActivityNewUserBinding;
 import com.facebook.Profile;
 
+import java.io.File;
+
 public class NewUserActivity extends AppCompatActivity {
+    // TODO: change all static constants to final
     private static String TAG = NewUserActivity.class.getCanonicalName();
+    private static int ACTION_IMAGE_CAPTURE_REQUEST_CODE = 2;
+    private static String PHOTO_FILE_NAME = "photo.png";
+    private File photoFile;
     private ActivityNewUserBinding binding;
 
     @Override
@@ -36,7 +46,55 @@ public class NewUserActivity extends AppCompatActivity {
         }
 
         binding.btContinue.setOnClickListener(this::continueOnClick);
+        binding.clChooseProfilePic.setOnClickListener(this::launchCamera);
+    }
 
+    private File getPhotoFileUri() {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "getPhotoFileUri: failed to create directory");
+        }
+
+        return new File(mediaStorageDir.getPath() + File.separator + PHOTO_FILE_NAME);
+    }
+
+    private void launchCamera(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri();
+
+        // wrap File object into a content provider
+        Uri fileProvider = FileProvider.getUriForFile(
+                this,
+                "com.ethanjhowell.friendly.fileprovider",
+                photoFile
+        );
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // check that there is a camera app that can handle the intent
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, ACTION_IMAGE_CAPTURE_REQUEST_CODE);
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ACTION_IMAGE_CAPTURE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Glide.with(this)
+                        .load(photoFile)
+                        .circleCrop()
+                        .into(binding.ivProfilePic);
+            }
+        } else
+            super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void loadFacebookImage(Profile profile) {
