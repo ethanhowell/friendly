5cc26206188e39f93a8b3fb352b8158ecaddbe59
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-14 12:33:19 -0500

    successfully loading in facebook profile pic

--- app/src/main/java/com/ethanjhowell/friendly/activities/NewUserActivity.java
+++ app/src/main/java/com/ethanjhowell/friendly/activities/NewUserActivity.java
@@ -1,12 +1,16 @@
 package com.ethanjhowell.friendly.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.ethanjhowell.friendly.databinding.ActivityNewUserBinding;
+import com.facebook.Profile;
 
 public class NewUserActivity extends AppCompatActivity {
+    private static String TAG = NewUserActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,16 @@ public class NewUserActivity extends AppCompatActivity {
         ActivityNewUserBinding binding = ActivityNewUserBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Profile currentProfile = Profile.getCurrentProfile();
+        if (currentProfile == null) {
+            Log.d(TAG, "onCreate: logged in via parse");
+        } else {
+            Log.d(TAG, "onCreate: logged in via facebook");
+            Glide.with(this)
+                    .load(currentProfile.getProfilePictureUri(300, 300).toString())
+                    .circleCrop()
+                    .into(binding.ivProfilePic);
+        }
 
     }
 }
