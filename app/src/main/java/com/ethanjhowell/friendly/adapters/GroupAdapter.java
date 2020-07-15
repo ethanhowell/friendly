cc6eec46f38725e21fbbe5671b4389c32178ead9
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-15 11:15:33 -0500

    using view binding instead of findviewbyid

--- app/src/main/java/com/ethanjhowell/friendly/adapters/GroupAdapter.java
+++ app/src/main/java/com/ethanjhowell/friendly/adapters/GroupAdapter.java
@@ -2,14 +2,13 @@ package com.ethanjhowell.friendly.adapters;
 
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.ethanjhowell.friendly.R;
+import com.ethanjhowell.friendly.databinding.ItemGroupBinding;
 import com.ethanjhowell.friendly.models.Group;
 
 import java.util.List;
@@ -25,8 +24,10 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.ViewHolder> 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        return new ViewHolder(inflater.inflate(R.layout.item_group, parent, false));
+        ItemGroupBinding binding = ItemGroupBinding.inflate(
+                LayoutInflater.from(parent.getContext()), parent, false
+        );
+        return new ViewHolder(binding);
     }
 
     @Override
@@ -42,10 +43,10 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.ViewHolder> 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvGroupName;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public ViewHolder(@NonNull ItemGroupBinding binding) {
+            super(binding.getRoot());
             // TODO: on click listeners to take us to the chat view
-            tvGroupName = itemView.findViewById(R.id.tvGroupName);
+            tvGroupName = binding.tvGroupName;
         }
 
         public void bind(Group group) {
