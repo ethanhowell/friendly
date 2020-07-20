015902497486373e5b583c3778c916e920b2df76
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-20 10:55:35 -0500

    created toolbar on got it properly displaying

--- app/src/main/java/com/ethanjhowell/friendly/activities/GroupActivity.java
+++ app/src/main/java/com/ethanjhowell/friendly/activities/GroupActivity.java
@@ -3,13 +3,16 @@ package com.ethanjhowell.friendly.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.ethanjhowell.friendly.R;
 import com.ethanjhowell.friendly.adapters.GroupAdapter;
 import com.ethanjhowell.friendly.databinding.ActivityGroupBinding;
 import com.ethanjhowell.friendly.models.Group;
@@ -61,6 +64,12 @@ public class GroupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityGroupBinding binding = ActivityGroupBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar.toolbar;
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+
+
         currentGroups = new ArrayList<>();
 
         FriendlyParseUser user = FriendlyParseUser.getCurrentUser();
@@ -87,6 +96,12 @@ public class GroupActivity extends AppCompatActivity {
         // click handler for floating action button
         binding.fabNewGroup.setOnClickListener(this::newGroupOnClick);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.group, menu);
+        return true;
     }
 
     @Override
